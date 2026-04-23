c6cc60e5a42da436b7ab35ba6b9a1ca131a07a78
aaronactive <88576011+aaronactive@users.noreply.github.com>
2026-04-23 14:51:35 +1000

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import altair as alt
 DB_FILE = "whisky.db"
 
 st.set_page_config(layout="wide")
-st.title("🥃 Whisky Auction Overview")
+st.title("🥃 Whisky Prices Overview")
 
 # =========================
 # FIX METRIC SIZE
